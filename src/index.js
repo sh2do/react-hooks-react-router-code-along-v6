80f9dca5c099d1c82b6097727f36cf37ec23d812
jeff <sddeh5981@gmsil.com>
2025-10-19 11:39:09 +0300

    uptade

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/login", element: <Login /> },
+]);
+
+root.render(<RouterProvider router={router} />);
